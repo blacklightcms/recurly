3dc3a4adef528b002554133c1dd5a21048c2935b
Kate Jefferson <barbedbella@gmail.com>
2017-01-26 22:25:06 -0500

    Updated version

--- client.go
+++ client.go
@@ -96,7 +96,7 @@ func (c *Client) newRequest(method string, action string, params Params, body in
 
 	req.Header.Set("Authorization", fmt.Sprintf("Basic %s", c.apiKey))
 	req.Header.Set("Accept", "application/xml")
-	req.Header.Set("X-Api-Version", "2.4")
+	req.Header.Set("X-Api-Version", "2.5")
 	if req.Method == "POST" || req.Method == "PUT" {
 		req.Header.Set("Content-Type", "application/xml; charset=utf-8")
 	}
