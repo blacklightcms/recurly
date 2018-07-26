3c3293d234b3f13e864a47c5172d768090556f76
W.Peterman <skix123@users.noreply.github.com>
2018-07-26 20:26:53 +0200

    Webhooks (#72)

---
+++ webhooks/dunning_events.go
@@ -0,0 +1,21 @@
+package webhooks
+
+import "github.com/blacklightcms/recurly"
+
+const (
+    NewDunningEvent = "new_dunning_event_notification"
+)
+
+type NewDunningEventNotification struct {
+    Type string `xml:"-"`
+    Account Account
+    Invoice ChargeInvoice
+    Subscription recurly.Subscription
+}
+
+type NewDunningEventDeprecatedNotification struct {
+    Type string `xml:"-"`
+    Account Account
+    Invoice Invoice
+    Subscription recurly.Subscription
+}
