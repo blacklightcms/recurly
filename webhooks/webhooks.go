726b070061aa837c6434fc4aced0979d99c44add
Denys Misko <denys@autopilothq.com>
2019-07-23 08:41:14 +1000

    Add closed_invoice_notification notification type

--- webhooks/webhooks.go
+++ webhooks/webhooks.go
@@ -55,7 +55,7 @@ func nameToNotification(name string) (interface{}, error) {
 	case NewSubscription, UpdatedSubscription, RenewedSubscription, ExpiredSubscription, CanceledSubscription, ReactivatedAccount, PausedSubscription,
 		ResumedSubscription, ScheduledPauseSubscription, ModifiedPauseSubscription, PausedRenewalSubscription, PauseCanceledSubscription:
 		return &SubscriptionNotification{Type: name}, nil
-	case NewChargeInvoice, ProcessingChargeInvoice, PastDueChargeInvoice, PastDueInvoice, PaidChargeInvoice, FailedChargeInvoice, ReopenedChargeInvoice:
+	case NewChargeInvoice, ProcessingChargeInvoice, PastDueChargeInvoice, PastDueInvoice, PaidChargeInvoice, FailedChargeInvoice, ReopenedChargeInvoice, ClosedInvoiceNotification:
 		return &ChargeInvoiceNotification{Type: name}, nil
 	case NewCreditInvoice, ProcessingCreditInvoice, ClosedCreditInvoice, VoidedCreditInvoice, ReopenedCreditInvoice, OpenCreditInvoice:
 		return &CreditInvoiceNotification{Type: name}, nil
