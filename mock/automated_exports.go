32af6a1a01786d4965eb08cc1b524ebf7b891246
Chou <aaroncho@8c859092f7df.ant.amazon.com>
2019-10-17 21:43:45 -0700

    add automated exports

---
+++ mock/automated_exports.go
@@ -0,0 +1,19 @@
+package mock
+
+import (
+	"context"
+	"github.com/aaronchooo/recurly"
+)
+
+var _ recurly.AutomatedExportsService = &AutomatedExportsService{}
+
+// AdjustmentsService manages the interactions for adjustments.
+type AutomatedExportsService struct {
+	OnGet      func(ctx context.Context, date string, fileName string) (*recurly.AutomatedExport, error)
+	GetInvoked bool
+}
+
+func (m *AutomatedExportsService) Get(ctx context.Context, date string, fileName string) (*recurly.AutomatedExport, error) {
+	m.GetInvoked = true
+	return m.OnGet(ctx, date, fileName)
+}
