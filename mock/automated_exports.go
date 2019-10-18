acffdf0bd56e3080510b5541b293b672045e4d4b
Chou <aaroncho@8c859092f7df.ant.amazon.com>
2019-10-18 16:58:26 -0700

    fix mock

--- mock/automated_exports.go
+++ mock/automated_exports.go
@@ -14,10 +14,10 @@ type AutomatedExportsService struct {
 	OnGet      func(ctx context.Context, date time.Time, fileName string) (*recurly.AutomatedExport, error)
 	GetInvoked bool
 
-	OnListDates      func(opts *recurly.PagerOptions) Pager
+	OnListDates      func(opts *recurly.PagerOptions) recurly.Pager
 	ListDatesInvoked bool
 
-	OnListFiles      func(date time.Time, opts *recurly.PagerOptions) Pager
+	OnListFiles      func(date time.Time, opts *recurly.PagerOptions) recurly.Pager
 	ListFilesInvoked bool
 }
 
@@ -26,12 +26,12 @@ func (m *AutomatedExportsService) Get(ctx context.Context, date time.Time, fileN
 	return m.OnGet(ctx, date, fileName)
 }
 
-func (m *AutomatedExportsService) ListDates(opts *recurly.PagerOptions) Pager {
+func (m *AutomatedExportsService) ListDates(opts *recurly.PagerOptions) recurly.Pager {
 	m.ListDatesInvoked = true
 	return m.OnListDates(opts)
 }
 
-func (m *AutomatedExportsService) ListFiles(date time.Time, opts *recurly.PagerOptions) Pager {
+func (m *AutomatedExportsService) ListFiles(date time.Time, opts *recurly.PagerOptions) recurly.Pager {
 	m.ListFilesInvoked = true
 	return m.OnListFiles(date, opts)
 }
